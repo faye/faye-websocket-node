e4d2bc62705f50aa6623ff9e25d6fadeb1a74ce5
James Coglan <jcoglan@gmail.com>
2011-11-24 23:55:54 +0000

    Add HTML test page, log incoming socket URL and version, and fix bugs in draft-76 parser.

--- examples/server.js
+++ examples/server.js
@@ -1,12 +1,23 @@
 var WebSocket = require('../lib/faye/websocket'),
+    fs        = require('fs'),
     http      = require('http');
 
 var port = process.argv[2] || 7000;
 
-var server = http.createServer();
+var server = http.createServer(function(request, response) {
+  var path = request.url;
+  
+  fs.readFile(__dirname + path, function(err, content) {
+    var status = err ? 404 : 200;
+    response.writeHead(status, {'Content-Type': 'text/html'});
+    response.write(content || 'Not found');
+    response.end();
+  });
+});
 
 server.addListener('upgrade', function(request, socket, head) {
   var ws = new WebSocket(request, socket, head);
+  console.log('open', ws.url, ws.version);
   
   ws.onmessage = function(event) {
     ws.send(event.data);
