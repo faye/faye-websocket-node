ee22f7595905a1545b0e316257bb2368b07bcef5
James Coglan <jcoglan@gmail.com>
2012-12-22 23:13:04 +0000

    Remove trailing whitespace.

--- examples/autobahn_client.js
+++ examples/autobahn_client.js
@@ -17,24 +17,24 @@ socket.onclose = function() {
     if (n > cases) {
       socket = new WebSocket.Client(host + '/updateReports?agent=' + encodeURIComponent(agent));
       socket.onclose = process.exit
-      
+
     } else if (skip.indexOf(n) >= 0) {
       runCase(n + 1);
-      
+
     } else {
       console.log('Running test case #' + n + ' ...');
       socket = new WebSocket.Client(host + '/runCase?case=' + n + '&agent=' + encodeURIComponent(agent));
-      
+
       socket.onmessage = function(event) {
         socket.send(event.data);
       };
-      
+
       socket.onclose = function() {
         runCase(n + 1);
       };
     }
   };
-  
+
   runCase(1);
 };
 
