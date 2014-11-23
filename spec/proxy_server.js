43ac0905941ab78f156730269e453159be1b5265
James Coglan <jcoglan@gmail.com>
2014-11-23 21:56:34 +0000

    Add vanilla request support to the proxy server.

--- spec/proxy_server.js
+++ spec/proxy_server.js
@@ -1,7 +1,11 @@
 var fs    = require('fs'),
     http  = require('http'),
     https = require('https'),
-    net   = require('net');
+    net   = require('net'),
+    url   = require('url');
+
+var AGENTS = {'http:': http, 'https:': https},
+    PORTS  = {'http:': 80,   'https:': 443};
 
 var ProxyServer = function(options) {
   var proxy = options.tls
@@ -12,6 +16,35 @@ var ProxyServer = function(options) {
             : http.createServer();
 
   options = options || {};
+
+  var onRequest = function(request, response) {
+    if (options.debug) console.log(request.method, request.url, request.headers);
+
+    var uri     = url.parse(request.url),
+        agent   = AGENTS[uri.protocol],
+        headers = {};
+
+    for (var key in request.headers) {
+      if (key.split('-')[0] !== 'proxy') headers[key] = request.headers[key];
+    }
+
+    var backend = agent.request({
+      method:  request.method,
+      host:    uri.hostname,
+      port:    uri.port || PORTS[uri.protocol],
+      path:    uri.path,
+      headers: headers,
+      rejectUnauthorized: false
+    });
+
+    request.pipe(backend);
+
+    backend.on('response', function(resp) {
+      if (options.debug) console.log(resp.statusCode, resp.headers);
+      response.writeHead(resp.statusCode, resp.headers);
+      resp.pipe(response);
+    });
+  };
 
   var onConnect = function(request, frontend, body) {
     var parts   = request.url.split(':'),
@@ -31,6 +64,7 @@ var ProxyServer = function(options) {
     backend.on( 'data', function(data) { console.log('O', data) });
   };
 
+  proxy.on('request', onRequest);
   proxy.on('connect', onConnect);
   proxy.on('upgrade', onConnect);
 
